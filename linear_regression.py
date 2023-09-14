9b35d1103f9884a5b5c31f03bedbe797319fe295
Nishtha <109549537+Nomadnp21@users.noreply.github.com>
2023-09-14 20:35:38 +0530

    Add files via upload

---
+++ linear_regression.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv(r"C:\Users\kkpan\OneDrive\Desktop\np\projects\USA_Housing.csv")
+df.head()
